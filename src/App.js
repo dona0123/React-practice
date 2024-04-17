e504392f52b37d36b37df25cec35ae8b7a13c6fc
dona <ehsk0123@naver.com>
2024-04-17 14:46:02 +0900

    ch03.3.3 Study

--- src/App.js
+++ src/App.js
@@ -1,44 +1,66 @@
-import './App.css';
-import Todo from './Todo';
-import React, { useState } from 'react';
-import  {Container, List, Paper } from "@mui/material";
-import AddTodo from './AddTodo';
+import "./App.css";
+import Todo from "./Todo";
+import React, { useState, useEffect } from "react";
+import { Container, List, Paper } from "@mui/material";
+import AddTodo from "./AddTodo";
 
 function App() {
   // item 상태 변수
   const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    };
+
+    fetch("http://localhost:8080/todo", requestOptions)
+      .then((response) => response.json())
+      .then(
+        (response) => {
+          setItems(response.data);
+        },
+        (error) => {}
+      );
+  }, []);
+
   const addItem = (item) => {
     item.id = "ID-" + items.length;
     item.done = false;
     setItems([...items, item]); // 자동으로 리렌더링이 일어남
-    console.log("items: ",items);
-  }
-
+    console.log("items: ", items);
+  };
 
   const deleteItem = (item) => {
-    const newItems = items.filter ( e => e.id !== item.id);
+    const newItems = items.filter((e) => e.id !== item.id);
     setItems([...newItems]);
-  }
-  
+  };
+
   const editItem = () => {
     setItems([...items]);
-  }
+  };
 
-  let todoItems = items.length > 0 && 
-    <Paper style={{ margin:16 }}>
+  let todoItems = items.length > 0 && (
+    <Paper style={{ margin: 16 }}>
       <List>
-        {items.map((item) => <Todo item={item} editItem={editItem} deleteItem={deleteItem} key={item.id}/> )};
-        
+        {items.map((item) => (
+          <Todo
+            item={item}
+            editItem={editItem}
+            deleteItem={deleteItem}
+            key={item.id}
+          />
+        ))}
+        ;
       </List>
     </Paper>
-  
+  );
 
   return (
     <div className="App">
       <Container maxWidth="md">
-          <AddTodo addItem={addItem} />
-          <div className='TodoList'>{todoItems}</div>
+        <AddTodo addItem={addItem} />
+        <div className="TodoList">{todoItems}</div>
       </Container>
     </div>
   );
